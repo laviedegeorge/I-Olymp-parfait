51071a188f439e1f51e9b183ce30fd878826a004
laviedegeorge <laviedegeorge1292@gmail.com>
2020-06-09 14:56:55 +0100

    first section of homepage

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let counter = 0;
+const slides = document.querySelectorAll('.item');
+const slidesCounter = document.querySelectorAll('.counter');
+
+const changeSlides = ()=>{
+    slides.forEach((el, i)=> {
+        if (i === counter) {
+           el.classList.add('active', 'fade');
+        } else {
+            el.classList.remove('active', 'fade')
+        }
+    });
+
+    if (counter === slides.length-1) {
+        counter = 0
+    }else {
+        counter ++;
+    }
+}
+
+setInterval(() => {
+    changeSlides();
+}, 5000);
